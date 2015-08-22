1cf868fe52309779a885c2dea171721fb91a66f2
Marek Tihkan <marektihkan@gmail.com>
2015-08-22 15:26:53 +0300

    Adds version for serializers

--- lib/mysterious/api/serialization.rb
+++ lib/mysterious/api/serialization.rb
@@ -2,11 +2,11 @@ module Mysterious
   module Api
     module Serialization
       def serialize(model, *mappings)
-        Serializers.find(*mappings).map(model)
+        V1::Serializers.find(*mappings).map(model)
       end
 
       def serialize_all(models, *mappings)
-        Serializers.find(*mappings).map_all(models)
+        V1::Serializers.find(*mappings).map_all(models)
       end
     end
   end
