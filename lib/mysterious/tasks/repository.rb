a654b5e7ea649a8c8d0101aac133e359095976de
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 21:10:20 +0300

    Adds tasks repository

---
+++ lib/mysterious/tasks/repository.rb
@@ -0,0 +1,30 @@
+module Mysterious
+  module Tasks
+    class Repository
+      include Spine::Hub::Publisher
+
+      def find(identity)
+        task = tasks.find_by(id: identity)
+        if task
+          publish(:task_found, task)
+        else
+          publish(:task_not_found, identity)
+        end
+        task
+      end
+
+      def all
+        result = tasks.all
+        publish(:tasks_found, result)
+        result
+      end
+
+      private
+
+      def tasks
+        Models::Task
+      end
+    end
+  end
+end
+
