63723b6cf2493a476bd4546eb3bdc9471da3c39c
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 22:53:03 +0300

    Filters user registration parameters

--- lib/mysterious/users/register.rb
+++ lib/mysterious/users/register.rb
@@ -4,13 +4,25 @@ module Mysterious
       include Spine::Hub::Publisher
 
       def call(attributes)
-        user = Models::User.new(attributes)
+        user = Models::User.new(filter(attributes))
         if user.save
           publish(:registered, user)
         else
           publish(:registration_failed, user)
         end
         user
+      end
+
+      private
+
+      def filter(attributes)
+        Filter.transform(attributes)
+      end
+
+      Filter = Spine::Transform.define do
+        copy :name
+        copy :email
+        copy :password
       end
     end
   end
