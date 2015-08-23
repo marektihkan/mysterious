7c605dcc4c8521ef0a4441a00e5cf0cbad526a09
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 20:17:59 +0300

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ module Mysterious
   Application.routes do
     scope :v1 do
       post :register, to: Api::V1::Users::Register
-      get :me, to: Api::V1::Users::Details
+      get :me, to: Api::V1::Users::Me
+
+      scope :users do
+        get ':id', to: Api::V1::Users::Details
+      end
     end
   end
 end
