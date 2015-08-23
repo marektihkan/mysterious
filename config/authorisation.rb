f3973e8ffbd314ab52345531263242b6fd484e68
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 22:22:08 +0300

    Removes vistor from authorisation

--- config/authorisation.rb
+++ config/authorisation.rb
@@ -1,8 +1,5 @@
 module Mysterious
   module Application
-    authorisation.permissions.define('visitor')
-      .grant(:read, :all)
-
     authorisation.permissions.define('user')
       .grant(:all, :all)
 
