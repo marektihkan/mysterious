d7490d77cda930fc4ff147f43a18866e2172f107
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 20:15:52 +0300

    Adds router parameters to action

--- lib/mysterious/api/v1/action.rb
+++ lib/mysterious/api/v1/action.rb
@@ -3,6 +3,7 @@ module Mysterious
     module V1
       class Action < Spine::Actions::Action
         include Spine::Hub::Subscriber
+        include Spine::Routing::Parameters
         include Spine::Parameters::Action
         include Responses
         include Authentication
