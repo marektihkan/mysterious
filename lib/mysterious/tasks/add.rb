29d772546a2877af56dffb2aa959c6edd8183825
Marek Tihkan <marektihkan@gmail.com>
2015-08-23 21:54:25 +0300

    Filters task attributes on add

--- lib/mysterious/tasks/add.rb
+++ lib/mysterious/tasks/add.rb
@@ -4,13 +4,26 @@ module Mysterious
       def call(attributes)
         return nil unless context.authorize(:write, :tasks)
 
-        task = context.user.tasks.build(attributes)
+        task = context.user.tasks.build(filter(attributes))
+
         if task.save
           publish(:task_added, task)
         else
           publish(:task_adding_failed, task)
         end
         task
+      end
+
+      private
+
+      def filter(attributes)
+        Filter.transform(attributes)
+      end
+
+      Filter = Spine::Transform.define do
+        copy :name
+        date :due
+        timestamp :completed_at
       end
     end
   end
